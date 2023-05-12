930d5cbe9f72134d358f28045d893d4849dff6f6
Jakub Ebertowski <jakub.ebertowski@outlook.com>
2023-05-13 00:41:30 +0200

    podstawy programu
    dodanie kilku parametrów, wybór pobudzenia
    testowe wprowadzanie wartości i wyświetalnie ich

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import tkinter as tk
+import tkinter.ttk
 import numpy as np
 
-window = tk.Tk()  # creating window
-window.geometry("1200x600") # sets size of window
+
+def get_digit():
+    digit = entry.get()
+    label.config(text=digit)
+    if digit.isdigit():
+        print("The entered digit is:", digit)
+    else:
+        print("Please enter a valid digit.")
+
+
+window = tk.Tk()  # implementacja okna
+window.geometry("1200x600")  # ustawienie wielkości okna
+
+# Parametry układu
+R = 0
+L = 0
+J = 0
+k = 0
+KT = 0
+zmienna = ''
+
+rodzaj_pobudzenia = tk.IntVar()
+rodzaj_pobudzenia.set(0)
+
+tk.Radiobutton(window, state='normal', text='Prostokątny', variable=rodzaj_pobudzenia, value=0).pack(anchor='nw', padx=0, pady=2)
+tk.Radiobutton(window, state='active', text='Trójkątny', variable=rodzaj_pobudzenia, value=1).pack(anchor='nw', padx=0, pady=4)
+tk.Radiobutton(window, state='normal', text='Harmoniczny', variable=rodzaj_pobudzenia, value=2).pack(anchor='nw', padx=0, pady=6)
+
+entry = tk.Entry(window)
+entry.pack()
+
+label = tk.Label(window, text='halo')
+
+button = tk.Button(window, text="Get Digit", command=get_digit)
+
+entry.pack()
+button.pack()
+label.pack()
 
 window.mainloop()
